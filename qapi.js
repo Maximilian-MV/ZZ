eba5e0e05bb0815123b2e897641921293fe9d9fd
Maximilian-MV <88027447+Maximilian-MV@users.noreply.github.com>
2021-08-04 13:14:14 +0300

    Changes

---
+++ qapi.js
@@ -0,0 +1,29 @@
+export default {
+	"pages": {
+		"root": {
+			"id": "root",
+			"pageUrl": "root",
+			"name": "root",
+			"children": [
+				"610a66ef208f73001fd892dd",
+				"610a66ef208f73001fd892df"
+			]
+		},
+		"610a66ef208f73001fd892dd": {
+			"id": "610a66ef208f73001fd892dd",
+			"name": "404",
+			"pageUrl": "404"
+		},
+		"610a66ef208f73001fd892df": {
+			"id": "610a66ef208f73001fd892df",
+			"name": "index",
+			"pageUrl": "index"
+		}
+	},
+	"mode": "production",
+	"projectType": "gatsby",
+	"site": {
+		"styles": {},
+		"seo": {}
+	}
+}
